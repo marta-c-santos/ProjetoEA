04b5b17c0e55c512181c468092bfcc298dafdfed
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2022-04-16 14:26:55 +0100

    Update main.cpp

--- H/main.cpp
+++ H/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 bool BFS(G, int v) {
@@ -24,14 +25,24 @@ bool BFS(G, int v) {
 
 int main() {
     //usar arraylist -> tipo lista
+    
     int n, m, id, estacao;
     bool final;
     while (cin >> n >> m) {
+        vector <vector<int>> teste(n);
+        vector <int> color(n, -1);
         if (n <= 500 and m <= 65000) {
             for (int i = 0; i < m; i++) {
                 cin >> id >> estacao;
-
+                teste[id - 1].push_back(estacao - 1);
+                teste[estacao - 1].push_back(id - 1);
             }
+            for (int j = 0; j < n; j++) {
+                if (color[j] == -1) {
+                    final = BFS(j, teste, color);
+                }
+            }
+
             final = BFS();
             if (final) {
                 cout << "NO\n";
@@ -41,5 +52,3 @@ int main() {
         }
     }
 
-    return 0;
-}
