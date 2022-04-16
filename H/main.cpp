54a13e5367c82faa2b78cad9297e123ed9c62afa
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2022-04-16 14:45:24 +0100

    Aceite

--- H/main.cpp
+++ H/main.cpp
@@ -3,34 +3,39 @@
 #include <queue>
 using namespace std;
 
-bool BFS(G, int v) {
+bool BFS(int v, vector<vector<int>> teste, vector <int> color) {
     //Q is an empty queue
-    color(v) = 1;
-    enqueue(v, Q);
-    while () { // Q nao esta vazio
-        int t = dequeue(Q);
-        for (int u = 0; u < ; u++) { //rever variaveis do for
-            if () { //u nao tem cor
-                color(u) = 1 - color(t);
-                enqueue(u, Q);
+    color[v] = 1;
+    queue <int> Q;
+    Q.push(v);
 
-            } else if (color(u) == color(t)){
+    while (!Q.empty()) { // Q nao esta vazio
+        int t = Q.front();
+        Q.pop();
+
+        int tam = teste[t].size();
+        for (int k = 0; k < tam; k++) { //rever variaveis do for
+            int u = teste[t][k];
+            if (color[u] == -1) { //u nao tem cor
+                color[u] = 1 - color[t];
+                Q.push(u);
+
+            } else if (color[u] == color[t]){
                 return false;
             }
         }
-
     }
     return true;
 }
 
 int main() {
-    //usar arraylist -> tipo lista
-    
     int n, m, id, estacao;
-    bool final;
+    bool final = false;
+
     while (cin >> n >> m) {
         vector <vector<int>> teste(n);
         vector <int> color(n, -1);
+
         if (n <= 500 and m <= 65000) {
             for (int i = 0; i < m; i++) {
                 cin >> id >> estacao;
@@ -40,15 +45,35 @@ int main() {
             for (int j = 0; j < n; j++) {
                 if (color[j] == -1) {
                     final = BFS(j, teste, color);
+                    if (!final)
+                        break;
                 }
+                
             }
 
-            final = BFS();
-            if (final) {
+            if (!final) {
                 cout << "NO\n";
             } else {
                 cout << "NOT SURE\n";
             }
         }
     }
+    return 0;
+}
 
+/*
+5 4
+1 2
+1 4
+1 5
+2 3
+5 5
+1 3
+2 4
+2 5
+3 4
+4 5
+
+NOT SURE
+NO
+*/
